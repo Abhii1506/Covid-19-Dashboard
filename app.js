4d01c1c0c9b97af312b9f67d3e1c6bebb19cd443
Anmol Pant <anmolpant@gmail.com>
2020-05-18 23:07:49 +0530

    added map and covid-19 clusters

--- app.js
+++ app.js
@@ -204,6 +204,194 @@ function populateLocations() {
     .forEach(([country_code, country]) => populateLocation(country, country_code));
 }
 
+mapboxgl.accessToken = 'add token here';
+function renderMap() {
+    mapboxgl.accessToken = 'add token here';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/dark-v10',
+center: [-103.59179687498357, 40.66995747013945],
+zoom: 3
+});
+ 
+map.on('load', function() {
+// Add a new source from our GeoJSON data and
+// set the 'cluster' option to true. GL-JS will
+// add the point_count property to your source data.
+map.addSource('places', {
+type: 'geojson',
+// Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+// from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
+data: {
+    type : "FeatureCollection",
+    crs: {
+        type: 'name',
+        properties: {
+            name: 'urn:ogc:def:crs:OGC:1.3:CRS84',
+        },
+    },
+    features: coronaData.locations.map(location =>{
+        //do reverse geocoding to obtain the place name
+        return{
+            type: 'Feature',
+            properties: {
+                description: `
+                    <table>
+                        <thead>
+                            <tr>Place Name</tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Confirmed Cases: </td>
+                                <td>${location.latest.confirmed}</td>
+                            </tr>
+                            <tr>
+                                <td>Deaths: </td>
+                                <td>${location.latest.deaths}</td>
+                            </tr>
+                            <tr>
+                                <td>Latitude: </td>
+                                <td>${location.coordinates.latitude}</td>
+                            </tr>
+                            <tr>
+                                <td>Longitude: </td>
+                                <td>${location.coordinates.longitude}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                
+                ` ,
+                icon: 'rocket'
+            },
+
+            geometry: {
+              type: "Point",
+              coordinates: [
+                  `${location.coordinates.longitude}`,
+                  `${location.coordinates.latitude}`
+              ]
+            }
+        };
+
+
+    })
+
+},
+cluster: true,
+clusterMaxZoom: 14, // Max zoom to cluster points on
+clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+});
+ 
+map.addLayer({
+id: 'clusters',
+type: 'circle',
+source: 'places',
+filter: ['has', 'point_count'],
+paint: {
+// Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+// with three steps to implement three types of circles:
+//   * Blue, 20px circles when point count is less than 100
+//   * Yellow, 30px circles when point count is between 100 and 750
+//   * Pink, 40px circles when point count is greater than or equal to 750
+'circle-color': [
+'step',
+['get', 'point_count'],
+'#51bbd6',
+100,
+'#f1f075',
+750,
+'#f28cb1'
+],
+'circle-radius': [
+'step',
+['get', 'point_count'],
+20,
+100,
+30,
+750,
+40
+]
+}
+});
+ 
+map.addLayer({
+id: 'cluster-count',
+type: 'symbol',
+source: 'places',
+filter: ['has', 'point_count'],
+layout: {
+'text-field': '{point_count_abbreviated}',
+'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+'text-size': 12
+}
+});
+ 
+map.addLayer({
+id: 'unclustered-point',
+type: 'circle',
+source: 'places',
+filter: ['!', ['has', 'point_count']],
+paint: {
+'circle-color': '#11b4da',
+'circle-radius': 4,
+'circle-stroke-width': 1,
+'circle-stroke-color': '#fff'
+}
+});
+ 
+// inspect a cluster on click
+map.on('click', 'clusters', function(event) {
+const features = map.queryRenderedFeatures(event.point, {
+layers: ['clusters']
+});
+const clusterId = features[0].properties.cluster_id;
+map.getSource('places').getClusterExpansionZoom(
+clusterId,
+function(error, zoom) {
+if (error) return;
+ 
+map.easeTo({
+center: features[0].geometry.coordinates,
+zoom: zoom
+});
+}
+);
+});
+ 
+// When a click event occurs on a feature in
+// the unclustered-point layer, open a popup at
+// the location of the feature, with
+// description HTML from its properties.
+map.on('click', 'unclustered-point', function(event) {
+const coordinates = event.features[0].geometry.coordinates.slice();
+//var mag = e.features[0].properties.mag;
+const { description } = event.features[0].properties;
+
+ 
+
+ 
+// Ensure that if the map is zoomed out such that
+// multiple copies of the feature are visible, the
+// popup appears over the copy being pointed to.
+while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
+coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360;
+}
+ 
+new mapboxgl.Popup()
+.setLngLat(coordinates)
+.setHTML(description)
+.addTo(map);
+});
+ 
+map.on('mouseenter', 'clusters', function() {
+map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'clusters', function() {
+map.getCanvas().style.cursor = '';
+});
+})
+}
+
 function populateLocation(country, country_code){
     const countryOption = document.createElement('option');
     countryOption.value = country;
@@ -226,6 +414,7 @@ async function initializeApp() {
     //renderUI(coronaData.latest, world = true);
     //console.log('Corona Data World Details', coronaData.latest);
     //console.log(`Corona Prone Locations: ${coronaData.locations}`);
+    renderMap();
     NProgress.done();
 
 }
